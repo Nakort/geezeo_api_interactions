de72091e4139715ba336a558d56e363f5162f547
nakort <nakort@gmail.com>
2013-06-21 15:36:28 -0430

    Initialize new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geezeo::Application.config.secret_token = '446731372dc91f36cbfbd4a6436ab14e63ea3bc2f5cc69b7e047d72dac8eefb418c9d43fd8335ef05a1b35f9a7542f90cc156405c69dadc6613619c61db58e51'
